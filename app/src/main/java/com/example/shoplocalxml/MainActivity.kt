2e4f5774619d0d98904049c82470a69bd1905018
Admin <author-main@mail.ru>
2023-05-04 21:49:58 +0300

    04.05.2023 Create App

--- app/src/main/java/com/example/shoplocalxml/MainActivity.kt
+++ app/src/main/java/com/example/shoplocalxml/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.shoplocalxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
