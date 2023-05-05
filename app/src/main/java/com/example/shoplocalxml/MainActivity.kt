24b166552def457be71b4801822b7bbf509286dd
Admin <author-main@mail.ru>
2023-05-05 12:52:52 +0300

    04.05.2023 Create App

--- app/src/main/java/com/example/shoplocalxml/MainActivity.kt
+++ app/src/main/java/com/example/shoplocalxml/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.shoplocalxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
