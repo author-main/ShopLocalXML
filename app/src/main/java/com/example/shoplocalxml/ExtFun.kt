3def980c59c52bc7d0d7ef7e6356af85e9a9497b
author-main <author-main@mail.ru>
2023-05-10 11:11:40 +0300

    05.05.2023 реализация Frame Login

---
+++ app/src/main/java/com/example/shoplocalxml/ExtFun.kt
@@ -0,0 +1,19 @@
+package com.example.shoplocalxml
+
+import android.content.res.Resources
+import android.util.Log
+
+val Int.toDp: Int
+    get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+
+val Int.toPx: Int
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun<T> log(value: T?) {
+    if (value != null) {
+        if (value is String)
+            Log.v("shoplocal", value)
+        else
+            Log.v("shoplocal", value.toString())
+    }
+}
--- app/src/main/java/com/example/shoplocalxml/custom_view/EditTextExt.kt
+++ app/src/main/java/com/example/shoplocalxml/custom_view/EditTextExt.kt
@@ -2,17 +2,31 @@ package com.example.shoplocalxml.custom_view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import com.example.shoplocalxml.log
+import com.example.shoplocalxml.toDp
+import com.example.shoplocalxml.toPx
 
 class EditTextExt(context: Context, attributes: AttributeSet) : AppCompatEditText(context, attributes) {
+    private val paint = Paint()
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+      /*  val width  = MeasureSpec.getSize(widthMeasureSpec)  + 2.toPx
+        val height = MeasureSpec.getSize(heightMeasureSpec) + 2.toPx
+        setMeasuredDimension(width, height)*/
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        /*canvas?.drawText(
+      /*  paint.color = Color.BLUE
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 2.toPx.toFloat()
+
+        canvas?.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), 8.toPx.toFloat(), 8.toPx.toFloat(), paint)
+        canvas?.drawText(
             prefix, defaultLeftPadding,
             getLineBounds(0, null).toFloat(), paint
         )*/
