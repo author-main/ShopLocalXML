6e72cd2e3260a44d7c6747d3c0b92dd8b4c15f4e
author-main <author-main@mail.ru>
2023-05-23 13:19:37 +0300

    21.05.2023 реализация Login

---
+++ app/src/main/java/com/example/shoplocalxml/SharedViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.shoplocalxml
+
+import androidx.lifecycle.ViewModel
+import com.example.shoplocalxml.repository.Repository
+
+class SharedViewModel(private val repository: Repository): ViewModel() {
+}
