9381c09013d7bf17dfb26a3e0d6787dea0289f4c
Admin <author-main@mail.ru>
2023-05-30 20:18:28 +0300

    26.05.2023 реализация LoginFragment

---
+++ app/src/main/java/com/example/shoplocalxml/ui/product_card/ProductImagesAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.shoplocalxml.ui.product_card
+
+import android.graphics.Bitmap
+
+class ProductImagesAdapter(private val images: List<Bitmap?>, private val onClickItem: (index: Int) -> Unit) {
+}
