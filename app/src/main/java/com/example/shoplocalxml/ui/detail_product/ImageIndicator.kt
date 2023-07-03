6a0427ea7add7468d9e44d92312ee6cf61c1c7b9
author-main <author-main@mail.ru>
2023-07-03 13:02:09 +0300

    03.07.2023 реализация Product Detail

---
+++ app/src/main/java/com/example/shoplocalxml/ui/detail_product/ImageIndicator.kt
@@ -0,0 +1,30 @@
+package com.example.shoplocalxml.ui.detail_product
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.example.shoplocalxml.R
+
+class ImageIndicator: LinearLayout {
+    constructor(context: Context) : super(context) {}
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        context.obtainStyledAttributes(attrs, R.styleable.ImageIndicator).run {
+            count = getInt(R.styleable.ImageIndicator_countImages, 1)
+            selectedIndex = getInt(R.styleable.ImageIndicator_selectedIndex, 0)
+            recycle()
+        }
+    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+    var count = 1
+    var selectedIndex = 0
+        set (value) {
+            field = value
+            if (selectedIndex != value)
+                setSelectedIndex(value)
+
+        }
+    @JvmName("setSelectedIndex_")
+    private fun setSelectedIndex(vaue: Int){
+
+    }
+}
