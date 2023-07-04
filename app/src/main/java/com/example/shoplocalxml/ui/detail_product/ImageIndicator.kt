a6a9671e193d7acc2983352ba9301c283dd07934
author-main <author-main@mail.ru>
2023-07-04 08:41:39 +0300

    03.07.2023 реализация Product Detail

--- app/src/main/java/com/example/shoplocalxml/ui/detail_product/ImageIndicator.kt
+++ app/src/main/java/com/example/shoplocalxml/ui/detail_product/ImageIndicator.kt
@@ -3,6 +3,8 @@ package com.example.shoplocalxml.ui.detail_product
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.setPadding
 import com.example.shoplocalxml.R
 import com.example.shoplocalxml.getStringResource
 import com.example.shoplocalxml.toPx
@@ -13,11 +15,14 @@ class ImageIndicator: LinearLayout {
         context.obtainStyledAttributes(attrs, R.styleable.ImageIndicator).run {
             count = getInt(R.styleable.ImageIndicator_countImages, 1)
             selectedIndex = getInt(R.styleable.ImageIndicator_selectedIndex, 0)
+            sizeSym = getInt(R.styleable.ImageIndicator_sizeSymbol, 14)
             recycle()
         }
     }
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
-    val indicatorSym = getStringResource(R.string.fCharPassword)
+    private val arrayTextView = ArrayList<TextView>()
+    private var sizeSym = 14
+    private val sym = getStringResource(R.string.fCharPassword)
     val interval = 4.toPx
     var count = 1
     var selectedIndex = 0
@@ -27,8 +32,25 @@ class ImageIndicator: LinearLayout {
                 setSelectedIndex(value)
 
         }
+
+    init {
+        addTextView()
+    }
     @JvmName("setSelectedIndex_")
     private fun setSelectedIndex(vaue: Int){
 
+    }
+    private fun addTextView() {
+        val textView = TextView(context)
+        val layoutParams = LayoutParams(
+            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT
+        )
+        textView.layoutParams = layoutParams
+        textView.setPadding(0)
+        textView.minWidth = 0
+        textView.minHeight = 0
+        textView.text = sym
+        this.addView(textView)
+        arrayTextView.add(textView)
     }
 }
