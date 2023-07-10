b967f5dc135fe5a0153ea59b4b69b54a953f27fd
Admin <author-main@mail.ru>
2023-07-10 22:55:13 +0300

    09.07.2023 реализация Product Detail

--- app/src/main/java/com/example/shoplocalxml/ui/dialog/DialogReview.kt
+++ app/src/main/java/com/example/shoplocalxml/ui/dialog/DialogReview.kt
@@ -4,10 +4,20 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.example.shoplocalxml.R
+import com.example.shoplocalxml.setDialogStyle
 
 class DialogReview: DialogFragment() {
     private lateinit var dialog: AlertDialog
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        /*val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder
+            .setView(dataBinding.root)
+            .setNegativeButton(R.string.button_cancel, null)
+            .setPositiveButton(R.string.btn_send, null)
+        dialog = builder.create()
+        setDialogStyle(dialog, title = R.string.title_restore)
+        return dialog*/
         return super.onCreateDialog(savedInstanceState)
     }
 }
