f5f7a94caae7b012d2e3caf144d51c8833d0e5f2
author-main <author-main@mail.ru>
2023-08-07 13:28:53 +0300

    21.07.2023 implementation Images Viewer

--- app/src/main/java/com/example/shoplocalxml/ui/notifications/MessagesNotification.kt
+++ app/src/main/java/com/example/shoplocalxml/ui/notifications/MessagesNotification.kt
@@ -1,4 +1,31 @@
 package com.example.shoplocalxml.ui.notifications
 
-class MessagesNotification {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import androidx.core.app.NotificationManagerCompat
+import com.example.shoplocalxml.AppShopLocal.Companion.applicationContext
+import com.example.shoplocalxml.classes.UserMessage
+
+
+class MessagesNotification(private val messages: List<UserMessage>) {
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "APP_SHOPLOCAL"
+    }
+    init {
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        val channel = NotificationChannel(
+            CHANNEL_ID, "My channel",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel.description = "My channel description"
+        channel.enableLights(true)
+        channel.lightColor = Color.RED
+        channel.enableVibration(false)
+
+    }
+    fun notify(){
+
+    }
 }
